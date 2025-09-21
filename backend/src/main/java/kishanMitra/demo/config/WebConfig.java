e2eefc088c2f23157c35f28d847ca4d5efdb83cc
Parth singh <ps.kuntal2005@gmail.com>
2025-09-21 19:28:29 +0530

    Update WebConfig.java

--- backend/src/main/java/kishanMitra/demo/config/WebConfig.java
+++ backend/src/main/java/kishanMitra/demo/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Allow CORS for all endpoints starting with /api
-                .allowedOrigins("http://localhost:3000") // The address of your frontend app
+                .allowedOrigins("*") // Allow all origins for deployment
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true);
